1d468e1d1cc9cd8d8e3336b12225ee14b3d5c4f4
Diego Felipe Dias <diego.f.dias@outlook.com>
2021-02-17 10:02:09 -0300

    Reaproveitamento de steps pra mais um cenário

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -3,9 +3,10 @@ package runners;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 
-@CucumberOptions(plugin = "pretty", features = "src/test/java/")
+@CucumberOptions(plugin = "pretty", features = "src/test/java/", snippets = SnippetType.CAMELCASE, monochrome = true)
 @RunWith(Cucumber.class)
 public class Runner {
 
--- target/test-classes/Funcionalidade.feature
+++ target/test-classes/Funcionalidade.feature
@@ -9,3 +9,8 @@ Cenário: Contador
 Dado um contador que inicia em 15
 Quando o incremento em 3
 Então seu resultado final é 18
+
+Cenário: Outro contador
+Dado um contador que inicia em 20
+Quando o incremento em 8
+Então seu resultado final é 28
