5de8c3ba3db7897f5e073517a30ef29e68e651c1
Diego Felipe Dias <diego.f.dias@outlook.com>
2021-04-19 14:00:04 -0300

    Alteração do cenário de teste e procurando uma forma de superar os captchas

--- src/test/java/br/com/projeto/steps/LojaOnlineSteps.java
+++ src/test/java/br/com/projeto/steps/LojaOnlineSteps.java
@@ -8,9 +8,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
 
@@ -20,165 +20,71 @@ public class LojaOnlineSteps {
 
 	protected WebDriver driver;
 
-	@Quando("^eu abrir a aplicação$")
-	public void eu_abrir_a_aplicação() throws Throwable {
+	@Quando("^eu abro a página inicial da aplicação$")
+	public void euAbroAPáginaInicialDaAplicação() throws Throwable {
+		String url = "https://www.kabum.com.br/";
 		System.setProperty("webdriver.chrome.driver", path);
-		String urlDesejada = "https://www.kabum.com.br/";
 		driver = new ChromeDriver();
-		driver.get(urlDesejada);
+		driver.get(url);
 		driver.manage().window().maximize();
-		String urlAtual = driver.getCurrentUrl();
-		Assert.assertTrue(urlDesejada.equals(urlAtual));
 	}
 
-	@Quando("^clicar no botão de cadastro$")
-	public void clicar_no_botão_de_cadastro() throws Throwable {
-		String elementoEsperado = "//*[@id=\"li-login-usuario\"]/a";
-		WebDriverWait wait = new WebDriverWait(driver, 10);
-		WebElement element = driver.findElement(By.xpath(elementoEsperado));
-		element.click();
-		String urlEsperada = "https://www.kabum.com.br/cgi-local/site/login/login.cgi";
-		wait.until(ExpectedConditions.urlToBe(urlEsperada));
-		String urlAtual = driver.getCurrentUrl();
-		Assert.assertTrue(urlAtual.equals(urlEsperada));
-
-	}
-
-	@Quando("^inserir \"([^\"]*)\" como email de acesso$")
-	public void inserir_como_email_de_acesso(String arg1) throws Throwable {
-		WebElement element = driver.findElement(By.id("textfield"));
-		element.sendKeys(arg1);
-
-	}
-
-	@Quando("^digitar \"([^\"]*)\" como senha$")
-	public void digitarComoSenha(String arg1) throws Throwable {
-		WebElement element = driver.findElement(By.id("textfield2"));
-		element.sendKeys(arg1);
-	}
-
-	@Quando("^digitar \"([^\"]*)\" para confirmar a senha$")
-	public void digitarParaConfirmarASenha(String arg1) throws Throwable {
-		WebElement element = driver.findElement(By.id("textfield3"));
-		element.sendKeys(arg1);
-	}
-
-	@Quando("^inserir \"([^\"]*)\" como CPF$")
-	public void inserirComoCPF(String arg1) throws Throwable {
-		WebElement element = driver.findElement(By.id("cpfCnpj"));
-		element.sendKeys(arg1);
-	}
-
-	@Quando("^inserir \"([^\"]*)\"-\"([^\"]*)\" como CEP$")
-	public void inserirComoCEP(String arg1, String arg2) throws Throwable {
-		WebElement element = driver.findElement(By.id("textfield4"));
-		element.sendKeys(arg1);
-		WebElement element2 = driver.findElement(By.id("textfield5"));
-		element2.sendKeys(arg2);
-	}
-
-	@Quando("^clicar em cadastrar$")
-	public void clicar_em_cadastrar() throws Throwable {
-		WebElement element = driver.findElement(By.name("cadastrar"));
-		element.click();
-	}
-
-	@Quando("^inserir \"([^\"]*)\" como primeiro nome$")
-	public void inserirComoPrimeiroNome(String arg1) throws Throwable {
-		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-		WebElement element = driver.findElement(By.id("textfield9"));
-		element.sendKeys(arg1);
-	}
-
-	@Quando("^inserir \"([^\"]*)\" como segundo nome$")
-	public void inserirComoSegundoNome(String arg1) throws Throwable {
-		WebElement element = driver.findElement(By.id("textfield10"));
-		element.sendKeys(arg1);
-	}
-
-	@Quando("^inserir o RG \"([^\"]*)\"$")
-	public void inserirORG(String arg1) throws Throwable {
-		WebElement element = driver.findElement(By.name("rg_ie"));
-		element.sendKeys(arg1);
-	}
-
-	@Quando("^inserir \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" como data de nascimento$")
-	public void inserirComoDataDeNascimento(String arg1, String arg2, String arg3) throws Throwable {
-		WebElement element = driver.findElement(By.cssSelector("#select4"));
-		Select select = new Select(element);
-		select.selectByVisibleText(arg1);
-
-		WebElement element2 = driver.findElement(By.cssSelector("#select5"));
-		Select select2 = new Select(element2);
-		select2.selectByVisibleText(arg2);
-
-		WebElement element3 = driver.findElement(By.cssSelector("#select6"));
-		Select select3 = new Select(element3);
-		select3.selectByVisibleText(arg3);
-	}
-
-	@Quando("^inserir \"([^\"]*)\" \"([^\"]*)\" como telefone fixo$")
-	public void inserirComoTelefoneFixo(String arg1, String arg2) throws Throwable {
-		WebElement element = driver.findElement(By.name("telefone_ddd"));
-		element.sendKeys(arg1);
-
-		WebElement element2 = driver.findElement(By.name("telefone"));
-		element2.sendKeys(arg2);
-	}
-
-	@Quando("^inserir \"([^\"]*)\" \"([^\"]*)\" como telefone celular$")
-	public void inserirComoTelefoneCelular(String arg1, String arg2) throws Throwable {
-		WebElement element = driver.findElement(By.name("celular_ddd"));
-		element.sendKeys(arg1);
-
-		WebElement element2 = driver.findElement(By.name("celular"));
-		element2.sendKeys(arg2);
-	}
-
-	@Quando("^inserir \"([^\"]*)\" como número da residência$")
-	public void inserirComoNúmeroDaResidência(String arg1) throws Throwable {
-		WebElement element = driver.findElement(By.name("endereco_numero"));
-		element.sendKeys(arg1);
-	}
-
-	@Quando("^inserir \"([^\"]*)\" como complemento$")
-	public void inserirComoComplemento(String arg1) throws Throwable {
-		WebElement element = driver.findElement(By.name("endereco_complemento"));
-		element.sendKeys(arg1);
-	}
-
-	@Quando("^inserir \"([^\"]*)\" como ponto de referência$")
-	public void inserirComoPontoDeReferência(String arg1) throws Throwable {
-		WebElement element = driver.findElement(By.name("endereco_referencia"));
-		element.sendKeys(arg1);
-	}
-
-	@Quando("^concordar em receber newsletter$")
-	public void concordarEmReceberNewsletter() throws Throwable {
-		WebElement element = driver.findElement(By.name("newsletter"));
-		Select select = new Select(element);
-		select.selectByVisibleText("Sim");
-	}
-
-	@Quando("^indicar que conheceu a loja através do \"([^\"]*)\"$")
-	public void indicarQueConheceuALojaAtravésDo(String arg1) throws Throwable {
+	@Quando("^clico no link Login|cadastre-se$")
+	public void clicoEmNoLinkLoginCadastreSe() throws Throwable {
+		String linkText = "LOGIN | CADASTRE-SE";
+		WebElement element = driver.findElement(By.linkText(linkText));
 		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-		WebElement element = driver.findElement(By.name("origem"));
-		Select select = new Select(element);
-		select.selectByVisibleText(arg1);
-	}
-
-	@Quando("^concordar com os termos de uso$")
-	public void concordarComOsTermosDeUso() throws Throwable {
-		WebElement element = driver.findElement(By.name("aceite"));
 		element.click();
 	}
 
-	@Então("^clicar em concluir$")
-	public void clicarEmConcluir() throws Throwable {
-		WebElement element = driver.findElement(By.name("cadastrar"));
+	@Então("^a seção de login deve ser exibida$")
+	public void aSeçãoDeLoginDeveSerExibida() throws Throwable {
+		String elemento = "//*[@id=\"Table_3\"]/tbody/tr[1]/td/font/b/span/font";
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(elemento)));
+		WebElement element = driver.findElement(By.xpath(elemento));
+		String textoEsperado = "Já é cadastrado?";
+		String texto = element.getText();
+		Assert.assertEquals(textoEsperado, texto);
+	}
+
+	@Então("^insiro meu email \"([^\"]*)\"$")
+	public void insiroMeu(String arg1) throws Throwable {
+		WebElement element = driver.findElement(By.id("textfield12"));
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		element.sendKeys(arg1);
+	}
+
+	@Então("^insiro minha senha \"([^\"]*)\"$")
+	public void insiroMinha(String arg1) throws Throwable {
+		WebElement element = driver.findElement(By.id("textfield15"));
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		element.sendKeys(arg1);
+	}
+
+	@Então("^clico em entrar$")
+	public void clicoEmEntrar() throws Throwable {
+		WebElement element = driver.findElement(By.xpath("//*[@id=\"Table_3\"]/tbody/tr[2]/td/p[5]/input"));
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
 		element.click();
-		driver.manage().timeouts().implicitlyWait(3, null);
+	}
+
+	@Então("^a mensagem \"([^\"]*)\" deverá aparecer na página$")
+	public void aMensagemDeveráAparecerNaPágina(String arg1) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Então("^quando clicar na área do cliente$")
+	public void quandoClicarNaÁreaDoCliente() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Então("^a área do cliente será exibida$")
+	public void aÁreaDoClienteSeráExibida() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
 	}
 
 }
